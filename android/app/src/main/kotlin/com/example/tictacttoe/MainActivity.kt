3fb4f9d539b1c9b5cc93f2580d8a6df3abe0688a
Sefa Erkan <erkansefa138@gmail.com>
2023-04-19 18:58:48 +0300

    tic tac toe

---
+++ android/app/src/main/kotlin/com/example/tictacttoe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tictacttoe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:tictacttoe/ui/theme/color.dart';
+import 'package:tictacttoe/utils/game_logic.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
+    );
+  }
+}
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  String lastValue = "X";
+  bool gameOver = false;
+  List<int> scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+  int turn = 0;
+  String result = "";
+  Game game = Game();
+
+  @override
+  void initState() {
+    super.initState();
+    game.board = Game.initGameBoard();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double boardWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: MainColor.primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "It's ${lastValue} turn".toUpperCase(),
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 58,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            width: boardWidth,
+            height: boardWidth,
+            child: GridView.count(
+              crossAxisCount: Game.boardlenth ~/ 3,
+              padding: EdgeInsets.all(16),
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              children: List.generate(Game.boardlenth, (index) {
+                return InkWell(
+                  onTap: gameOver
+                      ? null
+                      : () {
+                          if (game.board![index] == "") {
+                            setState(() {
+                              game.board![index] = lastValue;
+                              turn++;
+                              gameOver = game.winnerCheck(
+                                  lastValue, index, scoreboard, 3);
+                              if (gameOver) {
+                                result = "$lastValue is the Winner";
+                              } else if (!gameOver && turn == 9) {
+                                result = "İt's a Draw!";
+                                gameOver = true;
+                              }
+                              if (lastValue == "X")
+                                lastValue = "O";
+                              else
+                                lastValue = "X";
+                            });
+                          }
+                        },
+                  child: Container(
+                    width: Game.blocSize,
+                    height: Game.blocSize,
+                    decoration: BoxDecoration(
+                      color: MainColor.secondaryColor,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(
+                      child: Text(
+                        game.board![index],
+                        style: TextStyle(
+                          color: game.board![index] == "X"
+                              ? Colors.blue
+                              : Colors.pink,
+                          fontSize: 64,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+          const SizedBox(height: 25),
+          Text(
+            result,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 54.0,
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              setState(() {
+                game.board = Game.initGameBoard();
+                lastValue = "X";
+                gameOver = false;
+                turn = 0;
+                result = "";
+                scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+              });
+            },
+            icon: Icon(Icons.replay),
+            label: Text("Repeat the Game"),
+          ),
+        ],
+      ),
+    );
+  }
+}
